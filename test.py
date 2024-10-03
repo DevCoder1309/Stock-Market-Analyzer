ed2aa2849f86894d2b141dc0d4d55fbc9b5cff3c
Ankit Mukherjee <mukherjeeankit370@gmail.com>
2024-10-03 22:05:19 +0530

    Process of AutoCommit in the Project

--- test.py
+++ test.py
@@ -21,7 +21,7 @@ try:
         if stock_price:
             current_price = stock_price.text.strip()
             if current_price != old_price:
-                # Any Changes shall be put in this file and hence 
+                # Any Changes 
                 print(f"Current Price: {current_price}")
                 old_price = current_price
             else:
