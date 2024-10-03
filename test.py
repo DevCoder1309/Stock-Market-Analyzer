71358b5b3c4f0e63bb3d0a15ebdc24900bfc4846
Ankit Mukherjee <mukherjeeankit370@gmail.com>
2024-10-03 21:45:13 +0530

    made the machine learning model for Reliance Industries limited for the month of october and looking at other trends

---
+++ test.py
@@ -0,0 +1,33 @@
+from selenium import webdriver
+from bs4 import BeautifulSoup
+import time
+
+driver = webdriver.Chrome()  
+
+old_price = None  # Use None to signify that we haven't set a price yet
+
+try:
+    url = 'https://www.equitypandit.com/historical-data/RELIANCE'
+    
+    while True:
+        driver.get(url)
+        
+        time.sleep(5)  
+        
+        soup = BeautifulSoup(driver.page_source, 'html.parser')
+
+        stock_price = soup.find('p', {'id': 'BSE_OPEN_PRICE'})
+
+        if stock_price:
+            current_price = stock_price.text.strip()
+            if current_price != old_price:
+                print(f"Current Price: {current_price}")
+                old_price = current_price
+            else:
+                print(f"Price remains the same: {current_price}")
+        else:
+            print("Stock price not found.")
+        
+        time.sleep(60) 
+finally:
+    driver.quit() 
