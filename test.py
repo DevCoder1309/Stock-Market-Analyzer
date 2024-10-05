98045ce77f150e7c4b7c35f527f102096c7c76d6
Ankit Mukherjee <mukherjeeankit370@gmail.com>
2024-10-05 21:56:52 +0530

    Process of AutoCommit in the Project

--- test.py
+++ test.py
@@ -21,7 +21,7 @@ try:
 
         stock_open_new_price = stock_open_price.text.strip()
         stock_open_new_high_low = stock_high_low_price.text.strip()
-        stock_new_volume = stock_vloume.text.strip()
+        stock_new_volume = stock_volume.text.strip()
 
         arr = list(stock_open_new_high_low.split(' / '))
         print(arr)
