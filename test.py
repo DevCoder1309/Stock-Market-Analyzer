c5efe851e279ba8af2c0f0e3c03b6d57ea7d6f17
Ankit Mukherjee <mukherjeeankit370@gmail.com>
2024-10-05 21:11:06 +0530

    Process of AutoCommit in the Project

--- test.py
+++ test.py
@@ -18,7 +18,7 @@ try:
 
         stock_open_price = soup.find('p', {'id': 'BSE_OPEN_PRICE'})
         stock_current_price = soup.find('h1', {'id': 'BSE_PRICE'})
-        stock_high_low = soup.find('p', {})
+        stock_high_low = soup.find('p', {'id': })
 
         if stock_open_price:
             stock_open_new_price = stock_open_price.text.strip()
