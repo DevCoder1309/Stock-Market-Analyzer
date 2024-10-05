603b48e3d6003e55c42ffa83f5f01d4274ce8fe7
Ankit Mukherjee <mukherjeeankit370@gmail.com>
2024-10-05 21:36:58 +0530

    Process of AutoCommit in the Project

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 from selenium import webdriver
 from bs4 import BeautifulSoup
 import time
-from stock_analysis_pred.ipynb import call_
+from stock_analysis_pred.ipynb import call_prediction
 
 driver = webdriver.Chrome()  
 
