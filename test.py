a091e55ff8c55f8477193df434a93e17e97bf67e
Ankit Mukherjee <mukherjeeankit370@gmail.com>
2024-10-05 21:26:43 +0530

    Process of AutoCommit in the Project

--- test.py
+++ test.py
@@ -22,7 +22,7 @@ try:
 
         arr = list(stock_open_new_high_low.split(' / '))
         print(arr)
-        stock_high
+        stock_high_price = arr[1]
         if (stock_open_price and stock_high_low_price):
             print(f"Current Price: {stock_open_new_price}, {stock_open_new_high_low}")
         else:
