7cfef0881730e59d926a4141109726fb8d6f454e
Ankit Mukherjee <mukherjeeankit370@gmail.com>
2024-10-05 21:27:35 +0530

    Process of AutoCommit in the Project

--- test.py
+++ test.py
@@ -25,7 +25,7 @@ try:
         stock_high_price = arr[1]
         stock_low_price = arr[0]
         if (stock_open_price and stock_high_low_price):
-            print(f"Current Price: {stock_open_new_price}, {stock_high_price}, {stock_high_}")
+            print(f"Current Price: {stock_open_new_price}, {stock_high_price}, {stock_low_price}")
         else:
             print("Stock price not found.")
         
